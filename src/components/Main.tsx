eefdfc662ab9fe5d50b80199ef0ea8b90e715392
Adnan Khurshid <adnankhurshid251@gmail.com>
2024-06-26 12:42:30 +0530

    changed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 
 const Main = () => {
     return (
         <div className="flex flex-col w-screen h-screen justify-start items-center">
             <div className="text-4xl font-bold">This is test</div>
-            <Image src='/images/judhajit.png' alt="test" width={200} height={200}/>
-            <Image src='/images/judhajit.png' alt="test" width={200} height={200}/>
+            <img src='/images/judhajit.png' alt="test" width={200} height={200}/>
+            <img src='/images/judhajit.png' alt="test" width={200} height={200}/>
         </div>
     );
 }
