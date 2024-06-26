e69bede365bc330a834d02facaef044ddc0ff62e
Adnan Khurshid <adnankhurshid251@gmail.com>
2024-06-26 11:15:31 +0530

    changed

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Main = () => {
+    return (
+        <div className="flex flex-col w-screen h-screen justify-start items-center">
+            <div className="text-4xl font-bold">This is test</div>
+            <Image src='/images/judhajit.png' alt="test" width={200} height={200}/>
+        </div>
+    );
+}
+
+export default Main;
