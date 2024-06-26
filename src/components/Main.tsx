f7d6084c4b5b667dd9e9e93dd78730fbc9c55791
Adnan Khurshid <adnankhurshid251@gmail.com>
2024-06-26 11:18:51 +0530

    changed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,7 @@ const Main = () => {
         <div className="flex flex-col w-screen h-screen justify-start items-center">
             <div className="text-4xl font-bold">This is test</div>
             <Image src='/images/judhajit.png' alt="test" width={200} height={200}/>
+            <Image src='/images/judhajit.png' alt="test" width={200} height={200}/>
         </div>
     );
 }
